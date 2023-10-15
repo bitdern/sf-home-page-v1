c91e0345ba9d3f172738e646b412fe47589afde9
bitdern <acnewell09@gmail.com>
2023-10-15 17:43:03 -0500

    able to deploy, but price wont render from the API. Maybe need to spec URL more?

--- force-app/main/default/lwc/btcPrice/btcPrice.js
+++ force-app/main/default/lwc/btcPrice/btcPrice.js
@@ -1,17 +1,22 @@
-import { LightningElement, track } from "lwc";
-import getContactList from "@salesforce/apex/btcPrice.getBTCPrice";
-
-export default class ApexImperativeMethod extends LightningElement {
-  @track BTCPrice;
-  @track error;
-
-  handleLoad() {
-    getBTCPrice()
-      .then((result) => {
-        this.BTCPrice = result;
+import { LightningElement } from "lwc";
+export default class bitcoinComponent extends LightningElement {
+  BTCVALUE;
+  lastUpdatedDateTime;
+  loading = true;
+  updateButton() {
+    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then((response) => response.json())
+      .then((data) => {
+        this.BTCVALUE = data.bpi.USD.rate_float; //display price
+        this.loading = false; // hide loading spinner
+        this.lastUpdatedDateTime = new Date().getTime();
+        console.log(this.BTCVALUE);
       })
       .catch((error) => {
-        this.error = error;
+        console.log(error);
       });
+  }
+  connectedCallback() {
+    this.updateButton();
   }
 }
