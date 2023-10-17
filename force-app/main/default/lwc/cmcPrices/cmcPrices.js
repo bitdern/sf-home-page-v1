00e9499be0545c089d198a7313d2fcc65f32c7a0
bitdern <acnewell09@gmail.com>
2023-10-17 12:20:56 -0500

    new component not rendering either

---
+++ force-app/main/default/lwc/cmcPrices/cmcPrices.js
@@ -0,0 +1,20 @@
+import { LightningElement, wire } from "lwc";
+import getLivePrices from "@salesforce/apex/CryptoPriceController.getLivePrices";
+
+const columns = [
+  { label: "Name", fieldName: "name" },
+  { label: "Symbol", fieldName: "symbol" },
+  { label: "Price", fieldName: "price" },
+  { label: "24h Change", fieldName: "percent_change_24h" }
+];
+
+export default class CmcPrices extends LightningElement {
+  columns = columns;
+
+  @wire(getLivePrices)
+  coinPrices;
+
+  get coinPricesDefined() {
+    return this.coinPrices !== undefined && this.coinPrices.data !== undefined;
+  }
+}
